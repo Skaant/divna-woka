2938c35e0935b5729c580bd3fce6231b3586a902
Rimarok <romaric.ruga@gmail.com>
2022-06-09 23:27:29 +0200

    Carto.tsx: integrate multiple elements

--- src/Carto.tsx
+++ src/Carto.tsx
@@ -1,61 +1,58 @@
 import React from "react";
+import Polygon from "./components/Polygon";
 import { CARTO_DATA } from "./data/carto.data";
+import { LAND_SHAPE_DATA } from "./data/shapes/land.shape.data";
+import { SMALLIE_SHAPE_DATA } from "./data/shapes/smallie.shape.data";
+import { STATIONS_SHAPES_DATA } from "./data/shapes/stations.shapes.data";
 
 const {
-  LONG_MIN,
-  LONG_MAX,
-  LAT_MIN,
-  LAT_MAX,
   MICRO_COORD_LONG_METERS,
   MICRO_COORD_LAT_METERS,
   PIXELS_BY_METER,
+  WIDTH,
+  HEIGHT,
+  LAT_LONG_METERS_PERCENTS,
 } = CARTO_DATA;
-const WIDTH =
-  (LONG_MAX - LONG_MIN) * 1000000 * MICRO_COORD_LONG_METERS * PIXELS_BY_METER;
-const HEIGHT =
-  (LAT_MAX - LAT_MIN) * 1000000 * MICRO_COORD_LAT_METERS * PIXELS_BY_METER;
-const LAT_LONG_RATIO = (LAT_MAX - LAT_MIN) / (LONG_MAX - LONG_MIN);
+
+const MICRO_COORD_LONG_VIEWPORT =
+  MICRO_COORD_LONG_METERS * PIXELS_BY_METER * 1000 * (100 / WIDTH);
+const MICRO_COORD_LAT_VIEWPORT =
+  MICRO_COORD_LAT_METERS *
+  PIXELS_BY_METER *
+  1000 *
+  (LAT_LONG_METERS_PERCENTS / HEIGHT);
 
 function Carto() {
-  const POINTS: [number, number][] = [
-    [44.518349, 5.675629],
-    [44.518978, 5.675117],
-    [44.519094, 5.675377],
-    [44.51939, 5.675862],
-  ];
   return (
     <svg
       width={WIDTH}
       height={HEIGHT}
-      viewBox={`0 0 100 ${LAT_LONG_RATIO * 100}`}
+      viewBox={`0 0 100 ${LAT_LONG_METERS_PERCENTS}`}
     >
-      {POINTS.map(([lat, long], index) => {
-        return (
-          <circle
-            cx={
-              (((long - LONG_MIN) *
-                1000000 *
-                MICRO_COORD_LONG_METERS *
-                PIXELS_BY_METER) /
-                WIDTH) *
-              100
-            }
-            cy={
-              100 -
-              (((lat - LAT_MIN) *
-                1000000 *
-                MICRO_COORD_LAT_METERS *
-                PIXELS_BY_METER) /
-                HEIGHT) *
-                100
-            }
-            r={1}
-            fill="#ccbbaa"
-          >
-            {lat} {LAT_MIN} {long} {LONG_MIN}
-          </circle>
-        );
-      })}
+      <Polygon name="Grand terrain sauvage" points={LAND_SHAPE_DATA} />
+      <Polygon name="Petit terrain agricole" points={SMALLIE_SHAPE_DATA} />
+      {Object.entries(STATIONS_SHAPES_DATA).map(([station, coords]) => (
+        <Polygon name={station} points={coords} fill={"#bb2"} />
+      ))}
+      <line
+        x1={(100 - MICRO_COORD_LONG_VIEWPORT) / 2}
+        y1={LAT_LONG_METERS_PERCENTS / 2}
+        x2={MICRO_COORD_LONG_VIEWPORT + (100 - MICRO_COORD_LONG_VIEWPORT) / 2}
+        y2={LAT_LONG_METERS_PERCENTS / 2}
+        stroke="#aabbcc"
+      />
+      <line
+        x1={50}
+        y1={(LAT_LONG_METERS_PERCENTS - MICRO_COORD_LAT_VIEWPORT) / 2}
+        x2={50}
+        y2={
+          MICRO_COORD_LAT_VIEWPORT +
+          (LAT_LONG_METERS_PERCENTS - MICRO_COORD_LAT_VIEWPORT) / 2
+        }
+        stroke="#aabbcc"
+      >
+        {MICRO_COORD_LAT_METERS * PIXELS_BY_METER * 1000}
+      </line>
     </svg>
   );
 }
