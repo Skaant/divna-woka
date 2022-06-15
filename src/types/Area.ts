6ae119cf07cfa9c7493131a11e171bb5b245789d
Rimarok <romaric.ruga@gmail.com>
2022-06-15 11:48:47 +0200

    types: Add Path & Area

---
+++ src/types/Area.ts
@@ -0,0 +1,7 @@
+import { Coords } from "./Coords";
+
+export type Area = {
+  id: string;
+  name: string;
+  coords: Coords[];
+};
---
+++ src/types/Path.ts
@@ -0,0 +1,7 @@
+import { Coords } from "./Coords";
+
+export type Path = {
+  id: string;
+  name: string;
+  coords: Coords[];
+};
