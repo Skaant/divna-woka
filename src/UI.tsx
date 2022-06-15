f4b0f1d0bf313f7de36294831486a30963cc58bc
Rimarok <romaric.ruga@gmail.com>
2022-06-15 14:14:08 +0200

    Add UI component for name display

---
+++ src/UI.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Selection } from "./types/Selection";
+
+function UI({ selection }: { selection: Selection }) {
+  return (
+    <div
+      style={{
+        position: "fixed",
+        top: 0,
+        height: "100vh",
+        left: 0,
+        width: "100vw",
+        pointerEvents: "none",
+      }}
+    >
+      {selection && selection.name}
+    </div>
+  );
+}
+
+export default UI;
