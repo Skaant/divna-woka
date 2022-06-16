f3c28abec3f7743c5e72fa2627248c86b4d17240
Rimarok <romaric.ruga@gmail.com>
2022-06-16 15:09:41 +0200

    types: Area contains Shapes

--- src/types/Area.ts
+++ src/types/Area.ts
@@ -1,9 +1,11 @@
 import { Coords } from "./Coords";
 import { Flora } from "./Flora";
+import { Shape } from "./unions/Shape";
 
 export type Area = {
   id: string;
   name: string;
   coords: Coords[];
   flora?: Flora;
+  contains: Shape[];
 };
---
+++ src/types/unions/Shape.ts
@@ -0,0 +1,5 @@
+import { Area } from "../Area";
+import { Path } from "../Path";
+import { Point } from "../Point";
+
+export type Shape = Point | Path | Area;
