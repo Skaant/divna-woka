86c6621e35651a815d05cc03ca945c82f21c4917
Rimarok <romaric.ruga@gmail.com>
2022-06-17 12:22:34 +0200

    types: add Plant optional props

--- src/types/Plant.ts
+++ src/types/Plant.ts
@@ -1,6 +1,11 @@
 import { Point } from "./Point";
 import { SPECIES } from "../data/species/species.enum";
+import { Count } from "./Count";
 
 export type Plant = Point & {
   species: SPECIES;
+  age?: Count;
+  /** Date, month, year of apparition */
+  apparition?: string;
+  status?: "performing" | "challenged";
 };
